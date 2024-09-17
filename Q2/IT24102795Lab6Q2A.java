73f64eb8c78dcf5b60696b7d84283c5ec80a0979
IT24102795 <it24102795@my.sliit.lk>
2024-09-17 22:05:38 +0530

    Add files via upload

---
+++ Q2/IT24102795Lab6Q2A.java
@@ -0,0 +1,12 @@
+public class IT24102795Lab6Q2A {
+public static void main(String[] args) {
+
+    int num = 1;
+
+    while (num <= 10) 
+    {
+        System.out.print(num + " ");
+        num = num + 1;
+    }
+}
+}
---
+++ Q2/IT24102795Lab6Q2C.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class IT24102795Lab6Q2C {
+    public static void main(String[] args) {
+
+        Scanner input =  new Scanner(System.in);
+
+        int num1,num2,num3,num4,num5,num6,num7,num8,num9,num10;
+        int sum;
+
+        System.out.print("Enter a number : ");
+        num1 = input.nextInt();
+
+        System.out.print("Enter a number : ");
+        num2 = input.nextInt();
+
+        System.out.print("Enter a number : ");
+        num3 = input.nextInt();
+
+        System.out.print("Enter a number : ");
+        num4 = input.nextInt();
+
+        System.out.print("Enter a number : ");
+        num5 = input.nextInt();
+
+        System.out.print("Enter a number : ");
+        num6 = input.nextInt();
+
+        System.out.print("Enter a number : ");
+        num7 = input.nextInt();
+
+        System.out.print("Enter a number : ");
+        num8 = input.nextInt();
+
+        System.out.print("Enter a number : ");
+        num9 = input.nextInt();
+
+        System.out.print("Enter a number : ");
+        num10 = input.nextInt();
+
+        sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+
+        System.out.println("\n");
+        System.out.println("The numbers you entered are : ");
+        System.out.print(num1 + " " + num2 + " " + num3 + " " + num4 + " " + num5 + " " + num6 +
+        " " + num7 + " " + num8 + " " + num9 +  " " + num10);
+        System.out.print("\n");
+        System.out.print("sum of the numbers : " + sum);
+    }
+}
